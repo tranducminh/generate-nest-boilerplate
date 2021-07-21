669aef6c8c23f615a25e98dfc698a94bb8080eb8
Trần Đức Minh <36166484+tranducminh@users.noreply.github.com>
2021-07-21 15:37:49 +0700

    feat: gn-2 add base security

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // by default limit 10 requests in 60s
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
